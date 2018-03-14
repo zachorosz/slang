a4674bd47b61f47194c24c30abdb6f57267daef2
Zach Orosz <zjo5002@gmail.com>
2018-03-13 20:48:54 -0400

    added interpreter files; evaluate.go and parser.go

---
+++ evaluate.go
@@ -0,0 +1,237 @@
+package slang
+
+import (
+	"fmt"
+)
+
+func evaluateListItems(lst List, env Env) ([]LangType, error) {
+	lstLen := int(lst.Len())
+	values := make([]LangType, lstLen)
+
+	head := lst.head
+	for n := 0; n < lstLen; n++ {
+		item, err := Evaluate(head.value, env)
+		if err != nil {
+			return nil, err
+		}
+		values[n] = item
+		head = head.next
+	}
+
+	return values, nil
+}
+
+func evaluateVectorItems(vec Vector, env Env) (Vector, error) {
+	values := make(Vector, len(vec))
+	for i, item := range vec {
+		value, err := Evaluate(item, env)
+		if err != nil {
+			return Vector{}, err
+		}
+		values[i] = value
+	}
+	return values, nil
+}
+
+// evaluateSequence evaluates each expression in the given list except the last expression. The tail
+// expression is returned to be evaluated.
+func evaluateBodyTCO(lst List, env Env) (LangType, error) {
+	tailN := lst.len - 1
+	head := lst.head
+	for n := 0; n < tailN; n++ {
+		_, err := Evaluate(head.value, env)
+		if err != nil {
+			return nil, err
+		}
+		head = lst.head.next
+	}
+	return lst.tail, nil
+}
+
+func evaluateExpr(expr LangType, env Env) (LangType, error) {
+	switch t := expr.(type) {
+	case List:
+		return evaluateListItems(t, env)
+	case Vector:
+		return evaluateVectorItems(t, env)
+	case Symbol:
+		return env.Get(t)
+	default:
+		return t, nil
+	}
+}
+
+// Evaluate evaluates an expression
+func Evaluate(expr LangType, env Env) (LangType, error) {
+	for {
+		form, isList := expr.(List)
+		if !isList {
+			return evaluateExpr(expr, env)
+		}
+
+		if form.Len() == 0 {
+			return expr, nil
+		}
+
+		operator := form.First()
+
+		// peek at first object in list to determine evaluation path.
+		// if first is a symbol, it can match against any of the special form cases below. A list
+		// should be evaluated in the default case and any other type is not able to be applied.
+		var first Symbol
+		switch t := operator.(type) {
+		case List:
+			// just break. will eval and apply in default case
+			break
+		case Symbol:
+			first = t
+		default:
+			return nil, fmt.Errorf("'%s' is not applicable", operator)
+		}
+
+		switch first {
+		case "define":
+			operands := form.Rest()
+
+			if operands.Len() < 1 {
+				return nil, fmt.Errorf("Invalid form for define")
+			}
+
+			defsym, isSymbol := operands.First().(Symbol)
+			if !isSymbol {
+				return nil, fmt.Errorf("First argument must be a symbol")
+			}
+
+			var defval LangType
+			var err error
+			if operands.Len() >= 3 {
+				// syntactic sugar for a procedure definition
+				// Usage: `(define <procedureName> [params...] body...)
+				procdef := operands.Rest()
+				params := procdef.First()
+				body := procdef.Rest().(List)
+
+				paramsVec, isVec := params.(Vector)
+				if !isVec {
+					return nil, fmt.Errorf("Second argument must be a vector for procedure definition")
+				}
+
+				defval, err = MakeLambda(&env, paramsVec, body)
+			} else {
+				defval, err = Evaluate(operands.Nth(2), env)
+			}
+
+			if err != nil {
+				return nil, err
+			}
+
+			env.Set(defsym, defval)
+
+			return defval, nil
+		case "lambda":
+			operands := form.Rest()
+
+			// Usage: `(lambda [...params] ...body)`
+			if operands.Len() < 2 {
+				return nil, fmt.Errorf("Invalid number of arguments - expected at least 2 arguments")
+			}
+
+			params, isList := operands.First().(Vector)
+			if !isList {
+				return nil, fmt.Errorf("First argument to lambda must be a vector")
+			}
+
+			body := operands.Rest().(List)
+
+			return MakeLambda(&env, params, body)
+		case "quote":
+			operands := form.Rest()
+
+			if operands.Len() != 1 {
+				return nil, fmt.Errorf("Invalid number of arguments - expected 1 argument")
+			}
+			return operands.First(), nil
+		// Tail-call optimized paths
+		case "begin":
+			operands := form.Rest()
+
+			if operands.Len() < 1 {
+				return nil, fmt.Errorf("Invalid form for begin")
+			}
+
+			body := operands.Rest().(List)
+
+			tail, err := evaluateBodyTCO(body, env)
+			if err != nil {
+				return nil, err
+			}
+
+			expr = tail
+		case "if":
+			operands := form.Rest()
+
+			if operands.Len() < 2 || operands.Len() > 3 {
+				return nil, fmt.Errorf("Invalid form for if")
+			}
+
+			predicate, err := Evaluate(operands.First(), env)
+			if err != nil {
+				return nil, err
+			}
+
+			result, isBool := predicate.(bool)
+			if !isBool {
+				return nil, fmt.Errorf(
+					"If predicate must evaluate to either %t or %t", true, false)
+			}
+
+			if result == true {
+				// TCO loop to evaluate consequent
+				expr = operands.Nth(2)
+			} else if operands.Len() == 3 {
+				// TCO loop to evaluate alternative if provided
+				expr = operands.Nth(3)
+			} else {
+				return false, nil
+			}
+			// loop to evaluate consequent or alternative
+		default:
+			// evaluate all items in form (list); first should be an applicable procedure/lambda
+			e, err := evaluateExpr(form, env)
+			if err != nil {
+				return nil, err
+			}
+
+			values := e.([]LangType)
+			procedure := values[0]
+			args := values[1:]
+
+			// apply lambda or subroutine
+			if lambda, isLambda := procedure.(Lambda); isLambda {
+				if len(args) != len(lambda.Args) {
+					return nil, fmt.Errorf("Incorrect number of arguments to apply lambda")
+				}
+
+				// modify env to reference the lambda's closure
+				env = lambda.Closure
+
+				// perform left to right bindings of lambda arguments
+				for i, bindValue := range args {
+					bindSymbol := lambda.Args[i].(Symbol)
+					env.Set(bindSymbol, bindValue)
+				}
+
+				// evaluate all items in body except the final expression
+				// last expression is set in for next TCO loop iteration to evaluate it
+				expr, err = evaluateBodyTCO(lambda.Body, env)
+				if err != nil {
+					return nil, err
+				}
+			} else if subr, isSubroutine := procedure.(Subroutine); isSubroutine {
+				return subr.Apply(args...)
+			} else {
+				return nil, fmt.Errorf("'%s' is not applicable", procedure)
+			}
+		}
+	}
+}
---
+++ parser.go
@@ -0,0 +1,171 @@
+package slang
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type reader interface {
+	peek() *string
+	next() *string
+}
+
+type tokenReader struct {
+	tokens   []string
+	position int
+}
+
+func (rdr *tokenReader) peek() *string {
+	if rdr.position >= len(rdr.tokens) {
+		return nil
+	}
+	return &rdr.tokens[rdr.position]
+}
+
+func (rdr *tokenReader) next() *string {
+	if rdr.position >= len(rdr.tokens) {
+		return nil
+	}
+	token := rdr.tokens[rdr.position]
+	rdr.position++
+	return &token
+}
+
+func tokenize(expr string) []string {
+	r := regexp.MustCompile(`[()\[\]']|;.*|"(?:\\.|[^\\"])*"|[^\s()']*`)
+	tokens := make([]string, 0)
+	for _, token := range r.FindAllString(strings.TrimSpace(expr), -1) {
+		if !strings.HasPrefix(token, ";") {
+			tokens = append(tokens, token)
+		}
+	}
+	return tokens
+}
+
+func parseAtom(rdr reader) (LangType, error) {
+	token := rdr.next()
+
+	if nAtom, err := strconv.ParseFloat(*token, 64); err == nil {
+		return Number(nAtom), nil
+	}
+
+	if strings.HasPrefix(*token, "\"") {
+		if strings.HasSuffix(*token, "\"") {
+			return (*token)[1:len(*token)], nil
+		}
+		return nil, fmt.Errorf(`Expected string to end with non-escaped double quotes '"'`)
+	}
+
+	if *token == "true" {
+		return true, nil
+	}
+
+	if *token == "false" {
+		return false, nil
+	}
+
+	if *token == "nil" {
+		return nil, nil
+	}
+
+	return Symbol(*token), nil
+}
+
+func parseList(rdr reader, open string, close string) (LangType, error) {
+	token := rdr.next()
+	if token == nil || *token != open {
+		return nil, fmt.Errorf("Expected sequence to begin with '%s'", open)
+	}
+
+	seq := make([]LangType, 0)
+	for token = rdr.peek(); true; token = rdr.peek() {
+		if token == nil {
+			return nil, fmt.Errorf("Unbalanced '%s%s'", open, close)
+		}
+		if *token == close {
+			break
+		}
+		form, err := parseForm(rdr)
+		if err != nil {
+			return nil, err
+		}
+		seq = append(seq, form)
+	}
+	// skip over closing delim
+	rdr.next()
+
+	return seq, nil
+}
+
+func parseSequence(rdr reader, open string, close string, seq Sequence) (Sequence, error) {
+	token := rdr.next()
+	if token == nil || *token != open {
+		return nil, fmt.Errorf("Expected sequence to begin with '%s'", open)
+	}
+
+	for token = rdr.peek(); true; token = rdr.peek() {
+		if token == nil {
+			return nil, fmt.Errorf("Unbalanced '%s%s'", open, close)
+		}
+		if *token == close {
+			break
+		}
+		form, err := parseForm(rdr)
+		if err != nil {
+			return nil, err
+		}
+		seq = seq.Append(form)
+	}
+	// skip over closing delim
+	rdr.next()
+
+	return seq, nil
+}
+
+func parseQuote(rdr reader) (List, error) {
+	rdr.next()
+
+	quoted, err := parseForm(rdr)
+	if err != nil {
+		return List{}, err
+	}
+	//quoteForm := List{Symbol("quote"), quoted}
+	quoteForm := MakeList(Symbol("quote"), quoted)
+	return quoteForm, nil
+}
+
+func parseForm(rdr reader) (LangType, error) {
+	token := rdr.peek()
+	if token == nil {
+		return nil, nil
+	}
+
+	switch *token {
+	case "(":
+		lst, err := parseSequence(rdr, "(", ")", List{})
+		return lst, err
+	case ")":
+		return nil, fmt.Errorf("Unexpected ')'")
+	case "[":
+		vec, err := parseSequence(rdr, "[", "]", Vector{})
+		return vec, err
+	case "]":
+		return nil, fmt.Errorf("Unexpected ']'")
+	case "'":
+		return parseQuote(rdr)
+	default:
+		return parseAtom(rdr)
+	}
+}
+
+// Read tokenizes and parses an expression string into a domain language type.
+func Read(expr string) (LangType, error) {
+	tokens := tokenize(expr)
+	AST, err := parseForm(&tokenReader{tokens, 0})
+	if err != nil {
+		return nil, err
+	}
+	return AST, nil
+}
